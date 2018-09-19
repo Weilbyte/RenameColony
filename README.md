38287aeee732472e1701725ecd72800ef3f75a38
Weilbyte <imweilbyte@gmail.com>
2018-09-19 23:35:11 +0200

    Reupload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rename Colony
+
+Lets you rename your faction & base names at anytime! To use it simply click this little boye at the bottom-right part of the game!
+
+![alt text](https://steamuserimages-a.akamaihd.net/ugc/2434634953812994510/2D4DC34481F8DD8E065BD7718A5FC5EAFDCBF702/)
+
+# Installation
+Go to releases tab and download the latest release. Drop the 'Rename Colony' folder in RimWorld mods folder.
+
---
+++ Source/RenameColony.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace RenameColony
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            HarmonyInstance rchi = HarmonyInstance.Create("dusty.rimworld.renamecolony");
+            MethodInfo targetMethoderino = AccessTools.Method(typeof(PlaySettings), "DoPlaySettingsGlobalControls");
+            HarmonyMethod postFixerino = new HarmonyMethod(typeof(RenameColony).GetMethod("AddWidget"));
+            rchi.Patch(targetMethoderino, null, postFixerino);
+            Log.Message("[RenameColony] Postfixed");
+        }
+
+        public class RenameColony
+        {
+            public static void AddWidget(WidgetRow row, bool worldView)
+            {
+                if (worldView)
+                {
+                    #if DEBUG
+                    Log.Message("femboy foxes are gay");
+                    #endif
+                    return;
+                }
+                if (row.ButtonIcon(ContentFinder<Texture2D>.Get("Rename"), "Rename the Colony!"))
+                {
+                    Settlement settlement = (Settlement)Find.CurrentMap.info.parent;
+                    Find.WindowStack.Add(new Dialog_RenameColony(settlement));
+                    #if DEBUG
+                    Log.Message("femboy foxes are gay");
+                    #endif
+                }
+            }
+        }
+    }
+}
