d0f83bc6eec93f0d6597465a3ad468e7c6454e22
Weilbyte <git@weilbyte.net>
2020-03-13 14:09:12 +0100

    1.1 Update

---
+++ 1.1/Source/Dialog_RenameColony.cs
@@ -0,0 +1,57 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace RenameColony
+{
+    class Dialog_RenameColony : Dialog_GiveName
+    {
+        private Settlement settlement;
+
+        public Dialog_RenameColony(Settlement settlement)
+        {
+            this.settlement = settlement;
+            if (settlement.HasMap && settlement.Map.mapPawns.FreeColonistsSpawnedCount != 0)
+            {
+                Pawn suggestingPawn = settlement.Map.mapPawns.FreeColonistsSpawned.RandomElement<Pawn>();
+            }
+            this.curName = Faction.OfPlayer.Name;
+            this.nameMessageKey = "RCNamePlayerFactionMessage";
+            this.invalidNameMessageKey = "PlayerFactionNameIsInvalid";
+            this.useSecondName = true;
+            this.curSecondName = settlement.Name;
+            this.secondNameMessageKey = "RCNamePlayerFactionBaseMessage_NameFactionContinuation";
+            this.invalidSecondNameMessageKey = "RCPlayerFactionBaseNameIsInvalid";
+            this.gainedNameMessageKey = "RCPlayerFactionAndBaseGainsName";
+        }
+
+        public override void PostOpen()
+        {
+            base.PostOpen();
+            if (this.settlement.Map != null)
+            {
+                Current.Game.CurrentMap = this.settlement.Map;
+            }
+        }
+
+        protected override bool IsValidName(string s)
+        {
+            return NamePlayerFactionDialogUtility.IsValidName(s);
+        }
+
+        protected override bool IsValidSecondName(string s)
+        {
+            return NamePlayerSettlementDialogUtility.IsValidName(s);
+        }
+
+        protected override void Named(string s)
+        {
+            NamePlayerFactionDialogUtility.Named(s);
+        }
+
+        protected override void NamedSecond(string s)
+        {
+            NamePlayerSettlementDialogUtility.Named(this.settlement, s);
+        }
+    }
+}
---
+++ 1.1/Source/RenameColony.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace RenameColony
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            Harmony renameColony = new Harmony("weilbyte.rimworld.renamecolony");
+            MethodInfo targetMethod = AccessTools.Method(typeof(PlaySettings), "DoPlaySettingsGlobalControls");
+            HarmonyMethod postFix = new HarmonyMethod(typeof(RenameColony).GetMethod("AddWidget"));
+            renameColony.Patch(targetMethod, null, postFix);
+            Log.Message("RenameColony :: Postfixed");
+        }
+
+        public class RenameColony
+        {
+            public static void AddWidget(WidgetRow row, bool worldView)
+            {
+                if (worldView)
+                {
+                    return;
+                }
+                if (row.ButtonIcon(ContentFinder<Texture2D>.Get("Rename"), "Rename the Colony!"))
+                {
+                    Settlement settlement = (Settlement)Find.CurrentMap.info.parent;
+                    Find.WindowStack.Add(new Dialog_RenameColony(settlement));
+                }
+            }
+        }
+    }
+}
